2b47f676aa792142295f827d4b6d9e0cc23fd338
Daniel Ihenetu <ihenetudan@gmail.com>
2020-09-11 20:00:38 +0100

    Add setupRecyclerView method

--- app/src/main/java/com/example/gadsleaderboard/LeadersFragment.java
+++ app/src/main/java/com/example/gadsleaderboard/LeadersFragment.java
@@ -4,29 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A placeholder fragment containing a simple view.
- */
-public class PlaceholderFragment extends Fragment {
+public class LeadersFragment extends Fragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
 
-    private PageViewModel pageViewModel;
+    private ArrayList<Leader> leadersList = new ArrayList<>();
+    private LeadersAdapter leadersAdapter;
+    private RecyclerView leadersRecyclerView;
+    private LeadersViewModel leadersViewModel;
+    private int pageIndex;
 
-    public static PlaceholderFragment newInstance(int index) {
-        PlaceholderFragment fragment = new PlaceholderFragment();
+    public static LeadersFragment newInstance(int index) {
+        LeadersFragment fragment = new LeadersFragment();
         Bundle bundle = new Bundle();
         bundle.putInt(ARG_SECTION_NUMBER, index);
         fragment.setArguments(bundle);
@@ -36,34 +37,39 @@ public class PlaceholderFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
-        int index = 1;
+        leadersViewModel = ViewModelProviders.of(this).get(LeadersViewModel.class);
+        pageIndex = 1;
         if (getArguments() != null) {
-            index = getArguments().getInt(ARG_SECTION_NUMBER);
+            pageIndex = getArguments().getInt(ARG_SECTION_NUMBER);
         }
-        pageViewModel.setIndex(index);
     }
 
     @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
-        final TextView textView = root.findViewById(R.id.section_label);
-        final RecyclerView recyclerView = root.findViewById(R.id.leaders_recyclerview);
-        final LeadersAdapter adapter = new LeadersAdapter(getActivity(), new ArrayList<Leader>());
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        pageViewModel.getLearningHoursLeaders().observe(this, new Observer<List<Leader>>() {
+        leadersRecyclerView = root.findViewById(R.id.leaders_recyclerview);
+        leadersViewModel.getLeaders(pageIndex).observe(this, new Observer<List<Leader>>() {
             @Override
             public void onChanged(List<Leader> leaders) {
                 if ( leaders != null ) {
-                    textView.setText(leaders.toString());
-                    LeadersAdapter adapter = new LeadersAdapter(getActivity(), leaders);
-                    recyclerView.setAdapter(adapter);
+                    leadersList.addAll(leaders);
+                    leadersAdapter.notifyDataSetChanged();
                 }
             }
         });
+        setupRecyclerView();
         return root;
+    }
+
+    private void setupRecyclerView() {
+        if (leadersAdapter == null) {
+            leadersAdapter = new LeadersAdapter(getActivity(), leadersList, pageIndex);
+            leadersRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            leadersRecyclerView.setAdapter(leadersAdapter);
+            leadersRecyclerView.setItemAnimator(new DefaultItemAnimator());
+            leadersRecyclerView.setNestedScrollingEnabled(true);
+        } else {
+            leadersAdapter.notifyDataSetChanged();
+        }
     }
 }
