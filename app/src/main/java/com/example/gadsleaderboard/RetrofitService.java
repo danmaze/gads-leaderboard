c6a3e1b4b0fd66f2ce2e824359600e6d31ce13d4
Daniel Ihenetu <ihenetudan@gmail.com>
2020-09-11 20:03:57 +0100

    Add Retrofit service builder class

--- app/src/main/java/com/example/gadsleaderboard/RetrofitService.java
+++ app/src/main/java/com/example/gadsleaderboard/RetrofitService.java
@@ -1,4 +1,18 @@
 package com.example.gadsleaderboard;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitService {
+
+    private static Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(ApiUtil.getInstance().BASE_API_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+
+    public static <S> S createService(Class<S> serviceClass) {
+        return retrofit.create(serviceClass);
+    }
+
 }
