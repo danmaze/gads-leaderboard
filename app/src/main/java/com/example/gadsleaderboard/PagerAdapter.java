6e998c80e5b3524f9a7b1b1aac87940cf59ed9fd
Daniel Ihenetu <ihenetudan@gmail.com>
2020-09-11 20:03:14 +0100

    Remove unnecessary comments

--- app/src/main/java/com/example/gadsleaderboard/PagerAdapter.java
+++ app/src/main/java/com/example/gadsleaderboard/PagerAdapter.java
@@ -9,11 +9,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
-/**
- * A [FragmentPagerAdapter] that returns a fragment corresponding to
- * one of the sections/tabs/pages.
- */
-public class SectionsPagerAdapter extends FragmentPagerAdapter {
+public class PagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
     private static final int[] TAB_TITLES = new int[]{
@@ -22,7 +18,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     };
     private final Context mContext;
 
-    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+    public PagerAdapter(Context context, FragmentManager fm) {
         super(fm);
         mContext = context;
     }
@@ -30,9 +26,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        return LeadersFragment.newInstance(position + 1);
     }
 
     @Nullable
@@ -43,7 +37,6 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
         return 2;
     }
 }
