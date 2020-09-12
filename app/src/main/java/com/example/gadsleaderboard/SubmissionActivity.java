27a813cb35b7e0956da53dbc761b3e20fdec127f
Daniel Ihenetu <ihenetudan@gmail.com>
2020-09-12 19:58:38 +0100

    New activity for submitting project

--- app/src/main/java/com/example/gadsleaderboard/SubmissionActivity.java
+++ app/src/main/java/com/example/gadsleaderboard/SubmissionActivity.java
@@ -1,4 +1,15 @@
 package com.example.gadsleaderboard;
 
-public class SubmissionActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubmissionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submission);
+    }
+
 }
